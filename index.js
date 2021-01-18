4151d73b1f4ff1455fb3fd2407e1fa3d672f84ee
codesparsh <snowmansparsh4@gmail.com>
2021-01-18 16:53:15 +0530

    commiting the code

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+
 require('dotenv').config();
+const axios = require('axios');
+
+const express = require('express');
 const Discord = require('discord.js');
+const https = require('https');
 const bot = new Discord.Client();
 const TOKEN = process.env.TOKEN;
 
@@ -9,17 +14,24 @@ bot.on('ready', () => {
   console.info(`Logged in as ${bot.user.tag}!`);
 });
 
-bot.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-    msg.channel.send('pong');
+bot.on('message', async msg => {
 
-  } else if (msg.content.startsWith('!kick')) {
-    if (msg.mentions.users.size) {
-      const taggedUser = msg.mentions.users.first();
-      msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
-    } else {
-      msg.reply('Please tag a valid user!');
+    if (msg.content.startsWith('Bot!')) {
+      var url;
+      var str;
+      var content = msg.content.substring(4,msg.content.length);
+      content = content.split(' ').join('+');
+      console.log(content);
+      axios.get(`https://pixabay.com/api/?key=19929994-600d536324e19af54c13f167c&q=${content}`)
+      .then((res)=>{
+        var url;
+        url =res.data.hits[Math.floor(Math.random() * res.data.hits.length)]["webformatURL"];
+        msg.reply('Here\'s your image',{files:[url]})
+      }).catch(error => {
+        console.log(error);
+        msg.reply('There was an error');
+      });
     }
-  }
+    
 });
+
